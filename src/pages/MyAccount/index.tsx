e1454bed877289237551b5ced2ed0cb61db6d47b
KimJi-An <j0012a@naver.com>
2024-07-29 22:54:11 +0900

    chore: 5주차 과제 내용 가져오기

---
+++ src/pages/MyAccount/index.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+
+import { Button } from '@/components/common/Button';
+import { Spacing } from '@/components/common/layouts/Spacing';
+import { Wishlist } from '@/components/features/Wishlist';
+import { useAuth } from '@/provider/Auth';
+import { RouterPath } from '@/routes/path';
+import { authSessionStorage } from '@/utils/storage';
+
+export const MyAccountPage = () => {
+  const authInfo = useAuth();
+
+  const handleLogout = () => {
+    authSessionStorage.set(undefined);
+
+    const redirectURL = `${window.location.origin}${RouterPath.home}`;
+    window.location.replace(redirectURL);
+  };
+
+  return (
+    <Wrapper>
+      {authInfo?.name}님 안녕하세요! <Spacing height={64} />
+      <Button
+        size="small"
+        theme="darkGray"
+        onClick={handleLogout}
+        style={{
+          maxWidth: '200px',
+        }}
+      >
+        로그아웃
+      </Button>
+      <Spacing height={64} />
+      <WishlistWrapper>
+        <Wishlist />
+      </WishlistWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 80px 0 120px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 36px;
+`;
+
+const WishlistWrapper = styled.div`
+  width: 100%;
+  padding: 80px;
+`;
