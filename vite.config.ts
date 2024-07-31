4b5f0bbf2d3c55cdb054138819aa4974a351ed59
ajin <aj5210@naver.com>
2024-07-31 23:04:11 +0900

    fix: vite.config.ts 환경변수 로드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [
       react({
@@ -13,7 +15,7 @@ export default defineConfig(() => {
     ],
     server: {
       proxy: {
-          '/api': import.meta.env.VITE_API_BASE_URL,
+          '/api': env.VITE_API_BASE_URL,
       },
     },
   };
