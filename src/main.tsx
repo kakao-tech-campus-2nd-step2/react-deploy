2255e74a193b32e185dca8632bb4f56c8b5d94b4
ajin <aj5210@naver.com>
2024-07-31 16:52:37 +0900

    remove: 사용하지 않는 기능 제거
    - test, mock 관련 패키지, 파일 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -11,15 +11,8 @@ if (!rootElement) {
   throw new Error('Root element가 존재하지 않습니다.');
 }
 
-async function deferRender() {
-  const { worker } = await import('./mocks/browser');
-  return worker.start();
-}
-
-deferRender().then(() => {
-  ReactDOM.createRoot(rootElement).render(
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  );
-});
+ReactDOM.createRoot(rootElement).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
