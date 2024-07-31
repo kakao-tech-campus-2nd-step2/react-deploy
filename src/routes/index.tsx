dcf6d6cf6eb2fce35fc5a514805669671ce8034e
gimdogyun <jasper12394@gmail.com>
2024-07-31 16:27:44 +0900

    fix : add browserrouter basename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,54 +11,57 @@ import { ProductsPage } from '@/pages/Products';
 import { PrivateRoute } from './components/PrivateRoute';
 import { RouterPath } from './path';
 
-const router = createBrowserRouter([
-  {
-    path: RouterPath.root,
-    element: <Layout />,
-    children: [
-      {
-        path: RouterPath.home,
-        element: <HomePage />,
-      },
-      {
-        path: RouterPath.category,
-        element: <CategoryPage />,
-      },
-      {
-        path: RouterPath.products,
-        element: <ProductsPage />,
-      },
-      {
-        path: RouterPath.order,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.order,
-            element: <OrderPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.myAccount,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.myAccount,
-            element: <MyAccountPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.notFound,
-        element: <Navigate to={RouterPath.home} />,
-      },
-    ],
-  },
-  {
-    path: RouterPath.login,
-    element: <LoginPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: RouterPath.root,
+      element: <Layout />,
+      children: [
+        {
+          path: RouterPath.home,
+          element: <HomePage />,
+        },
+        {
+          path: RouterPath.category,
+          element: <CategoryPage />,
+        },
+        {
+          path: RouterPath.products,
+          element: <ProductsPage />,
+        },
+        {
+          path: RouterPath.order,
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: RouterPath.order,
+              element: <OrderPage />,
+            },
+          ],
+        },
+        {
+          path: RouterPath.myAccount,
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: RouterPath.myAccount,
+              element: <MyAccountPage />,
+            },
+          ],
+        },
+        {
+          path: RouterPath.notFound,
+          element: <Navigate to={RouterPath.home} />,
+        },
+      ],
+    },
+    {
+      path: RouterPath.login,
+      element: <LoginPage />,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL },
+);
 
 export const Routes = () => {
   return <RouterProvider router={router} />;
