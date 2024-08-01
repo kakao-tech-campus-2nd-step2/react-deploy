6f9088293bd2c9ec28a96914affc23e268119d6e
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-08-01 15:16:43 +0900

    chore: add eun kyeong base url

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { ERROR } from '@utils/constants/message';
 import { QueryClient } from '@tanstack/react-query';
 
@@ -12,15 +12,13 @@ const statusMessages: { [key: number]: string } = {
   500: ERROR.SERVER_ERROR,
 };
 
-export const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
+export const initInstance = (baseURL?: string): AxiosInstance => {
   const instance = axios.create({
     timeout: 5000,
-    baseURL: process.env.REACT_APP_BASE_URL,
-    ...config,
+    baseURL,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      ...config.headers,
     },
   });
 
@@ -38,8 +36,4 @@ export const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   return instance;
 };
 
-const axiosInstance = initInstance({});
-
 export const queryClient = new QueryClient();
-
-export default axiosInstance;
--- src/apis/members/hooks/useMemerLogin.ts
+++ src/apis/members/hooks/useMemerLogin.ts
@@ -1,12 +1,14 @@
-import axiosInstance from '@apis/instance';
+import { initInstance } from '@apis/instance';
 import { AxiosError } from 'axios';
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { UserInfoData } from '@internalTypes/dataTypes';
 import { MemberResponse } from '@internalTypes/responseTypes';
 import { MEMBERS_PATHS } from '@apis/path';
 
+const loginInstance = initInstance(process.env.REACT_APP_EUN_KYOUNG_BASE_URL);
+
 const postMemberLogin = async ({ email, password }: UserInfoData): Promise<MemberResponse> => {
-  const res = await axiosInstance.post(MEMBERS_PATHS.REGISTER, {
+  const res = await loginInstance.post(MEMBERS_PATHS.REGISTER, {
     email,
     password,
   });
