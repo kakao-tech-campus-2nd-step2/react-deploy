d3ede590e1f827f030f636fa298ab3e674e961c9
codus1718 <chaeyeon1718@daum.net>
2024-08-01 20:30:51 +0900

    step 0 - 기본 코드 준비

--- src/mocks/setupTests.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-import { worker } from './browser';
-
-beforeAll(() => worker.start());
-afterEach(() => worker.resetHandlers());
-afterAll(() => worker.stop());
