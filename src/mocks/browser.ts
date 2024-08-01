9cfe3d93f81cab4633bf8a33139958fa8ba60431
KimKyuHoi <k546kh@gmail.com>
2024-08-01 16:04:18 +0900

    Remove: mock 파일 제거

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -4,7 +4,6 @@ import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productDetailMockHandler } from '@/api/hooks/productDetail.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
 import { productOptionsMockHandler } from '@/api/hooks/productsOption.mock';
-import { loginMockHandler } from '@/api/hooks/login.mock';
 import { signupMockHandler } from '@/api/hooks/signup.mock';
 import { AddWishListMockHandler } from '@/api/hooks/addWishList.mock';
 import { FindWishListMockHandler } from '@/api/hooks/findWishList.mock';
@@ -15,7 +14,6 @@ export const worker = setupWorker(
   ...productsMockHandler,
   ...productDetailMockHandler,
   ...productOptionsMockHandler,
-  ...loginMockHandler,
   ...signupMockHandler,
   ...AddWishListMockHandler,
   ...FindWishListMockHandler,
