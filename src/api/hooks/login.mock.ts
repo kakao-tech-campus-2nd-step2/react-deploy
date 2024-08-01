362f2ffb2a55aa74c45a46605a586e504b52f9cf
hyunaeri <tjgus9650@gmail.com>
2024-08-01 23:41:39 +0900

    Feat: fetchInstance 를 사용하여 API 명세에 맞게 로그인 로직 수정

--- src/api/hooks/login.mock.ts
+++ src/api/hooks/login.mock.ts
@@ -3,27 +3,27 @@ import { rest } from 'msw';
 import { BASE_URL } from '../instance';
 
 type LoginMockRequest = {
-  id: string;
+  email: string;
   password: string;
 };
 
 export const loginMockHandler = [
   rest.post(`${BASE_URL}/api/members/login`, (req, res, ctx) => {
-    const { id, password } = req.body as LoginMockRequest;
+    const { email, password } = req.body as LoginMockRequest;
 
     // 임의의 아이디와 비밀번호로 로그인 성공
-    if (id === 'example@gmail.com' && password === 'example123') {
+    if (email === 'example@gmail.com' && password === 'example123') {
       return res(
         ctx.status(200),
         ctx.json({
-          id,
+          email: email,
           token: 'example-token',
         }),
       );
     }
 
     return res(
-      ctx.status(401),
+      ctx.status(400),
       ctx.json({
         message: '아이디 또는 비밀번호가 일치하지 않습니다.',
       }),
