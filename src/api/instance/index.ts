0e2fbcde67d542562de68ed28924d87c5d6813f0
joojjang <itchu999@naver.com>
2024-08-01 15:20:23 +0900

    feat(API): 요청 URL apiOption에 따라 변경

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -2,6 +2,24 @@ import { QueryClient } from '@tanstack/react-query';
 import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
+const BASE_URL_KIM = process.env.REACT_APP_BASE_URL_KIM;
+const BASE_URL_PARK = process.env.REACT_APP_BASE_URL_PARK;
+const BASE_URL_AHN = process.env.REACT_APP_BASE_URL_AHN;
+const BASE_URL_LEE = process.env.REACT_APP_BASE_URL_LEE;
+
+const apiOption = localStorage.getItem('apiOption') || '김은선';
+
+export const BASE_URL =
+  apiOption === '김은선'
+    ? BASE_URL_KIM
+    : apiOption === '박준석'
+      ? BASE_URL_PARK
+      : apiOption === '안재민'
+        ? BASE_URL_AHN
+        : apiOption === '이도훈'
+          ? BASE_URL_LEE
+          : BASE_URL_KIM;
+
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
     timeout: 5000,
@@ -16,10 +34,8 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   return instance;
 };
 
-export const BASE_URL = 'https://api.example.com';
-// TODO: 추후 서버 API 주소 변경 필요
 export const fetchInstance = initInstance({
-  baseURL: 'https://api.example.com',
+  baseURL: BASE_URL,
 });
 
 export const queryClient = new QueryClient({
