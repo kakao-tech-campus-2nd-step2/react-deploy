ae59b870ff597f45606cc509f7caa8456ba58b88
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-08-01 12:28:10 +0900

    refactor: 겹치는 api경로 상수처리

--- src/api/wish.ts
+++ src/api/wish.ts
@@ -1,8 +1,10 @@
 import { fetchInstance } from './instance';
 
+const BASE_PATH = '/api/wishes';
+
 export const addWish = async (productId: number) => {
   try {
-    const response = await fetchInstance.post('/api/wishes', { productId });
+    const response = await fetchInstance.post(`${BASE_PATH}`, { productId });
     return response.status === 201;
   } catch (error) {
     console.error('Error:', error);
@@ -12,7 +14,7 @@ export const addWish = async (productId: number) => {
 
 export const getWishList = async () => {
   try {
-    const response = await fetchInstance.get('/api/wishes');
+    const response = await fetchInstance.get(`${BASE_PATH}`);
     return response.data;
   } catch (error) {
     console.error('Error:', error);
@@ -22,7 +24,7 @@ export const getWishList = async () => {
 
 export const removeWish = async (wishId: number) => {
   try {
-    const response = await fetchInstance.delete(`/api/wishes/${wishId}`);
+    const response = await fetchInstance.delete(`${BASE_PATH}/${wishId}`);
     return response.status === 204;
   } catch (error) {
     console.error('Error:', error);
