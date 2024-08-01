5974a9ff122661e91cd58ac3ba857e7cea7103a5
KimJi-An <j0012a@naver.com>
2024-08-01 22:18:51 +0900

    fix: 위시리스트에서 상품이 삭제되지 않는 문제 해결

--- src/components/features/Wishlist/index.tsx
+++ src/components/features/Wishlist/index.tsx
@@ -1,12 +1,30 @@
 import { Box, Button, Heading, List, ListItem, Text } from '@chakra-ui/react';
+// import { useQueryClient } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 import { useGetWishlist } from '@/api/hooks/useGetWishlist';
 import { VisibilityLoader } from '@/components/common/VisibilityLoader';
 import { useAuth } from '@/provider/Auth';
 
+type WishlistItem = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+};
+
 export const Wishlist = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetWishlist({ maxResults: 10 });
   const authInfo = useAuth();
+  // const queryClient = useQueryClient(); 
+  const [wishlist, setWishlist] = useState<WishlistItem[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      const flattenWishlist = data.pages.flatMap((page) => page.content).flat();
+      setWishlist(flattenWishlist);
+    }
+  }, [data]);
 
   const handleDelete = async (wishId: number) => {
     
@@ -18,14 +36,12 @@ export const Wishlist = () => {
         },
       });
       if (response.status === 204) {
-        
+        setWishlist((prevWishlist) => prevWishlist.filter((item) => item.id !== wishId));
       } else {
         alert('삭제 중 오류가 발생했습니다.');
       }
     }
   };
-
-  const flattenWishlist = data?.pages.flatMap((page) => page.content).flat() || []; 
 
   return (
     <Box w="100%" display="flex" justifyContent="center">
@@ -33,11 +49,11 @@ export const Wishlist = () => {
         <Heading as="h2" fontSize="25px" mb={4}>
           관심 목록
         </Heading>
-        {flattenWishlist.length === 0 ? (
+        {wishlist.length === 0 ? (
           <Text fontSize="17px">관심 상품이 없습니다.</Text>
         ) : (
           <List spacing={3}>
-            {flattenWishlist.map((item) => 
+            {wishlist.map((item) => 
               <ListItem key={item.id} p={2} borderWidth={1} borderRadius="md">
                 <Box display="flex" justifyContent="space-between" alignItems="center">
                   <Box display="flex" alignItems="center" gap="30px">
