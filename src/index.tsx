752ce51f8a7a96ea8c874239ea33742fac5c26f9
faddishcorn <juni129461@gmail.com>
2024-08-03 01:14:55 +0900

    chore: msw 비활성화

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import { worker } from './mocks/browser';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
