621417d9133bb22a8bd40cf9e38876fde0909b72
dlawlghks <ikuwlghks@naver.com>
2024-08-03 03:15:21 +0900

    refactor: BrowserRouter basename 설정 제거

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,13 @@ import '@/styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// GitHub Pages의 서브디렉토리를 인식하도록 basename 설정
-const basename = process.env.PUBLIC_URL;
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
