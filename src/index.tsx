078f4f6f12460d8f3cabe385435525639d6d408a
dlawlghks <ikuwlghks@naver.com>
2024-08-02 23:21:10 +0900

    chore: 페이지 새로고침 문제해결

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import '@/styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+// GitHub Pages의 서브디렉토리를 인식하도록 basename 설정
+const basename = process.env.PUBLIC_URL;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
