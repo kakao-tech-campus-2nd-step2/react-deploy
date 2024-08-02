1d6ed6305fa3304089c73771cb4fafca56fcd925
HyeonChae1104 <211063@jnu.ac.kr>
2024-08-02 11:26:06 +0900

    feat: 회원(회원가입, 로그인)API 명세 반영

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,50 +1,15 @@
 import styled from '@emotion/styled';
-import axios from 'axios';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Button } from '@/components/common/Button';
-import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
-import { Spacing } from '@/components/common/layouts/Spacing';
+import RegisterForm from './RegisterForm';
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
-  const handleRegister = async () => {
-    if (!email || !password) {
-      alert('이메일과 비밀번호를 입력해주세요.');
-      return;
-    }
-
-    try {
-      const response = await axios.post('/api/members/register', {
-        email,
-        password,
-      });
-      localStorage.setItem('token', response.data.token);
-      localStorage.setItem('email', email);
-      alert('회원가입 및 로그인 성공');
-      navigate('/');
-    } catch (error) {
-      alert('회원가입 실패');
-    }
-  };
 
   return (
     <Wrapper>
       <FormWrapper>
-        <UnderlineTextField placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <Spacing />
-        <UnderlineTextField
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Spacing height={40} />
-        <Button onClick={handleRegister}>회원가입</Button>
+        <RegisterForm navigate={navigate} />
       </FormWrapper>
     </Wrapper>
   );
