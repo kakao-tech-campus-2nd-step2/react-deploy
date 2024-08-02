c329ce74729f8fbf89daa61ac3fe1b353b11c71a
faddishcorn <juni129461@gmail.com>
2024-08-03 01:17:32 +0900

    chore: 로그인 mock 수정

--- src/api/hooks/login.mock.ts
+++ src/api/hooks/login.mock.ts
@@ -1,27 +1,14 @@
 import { rest } from 'msw';
 import { BASE_URL } from '../instance';
-import { userSessionStorage } from '@/utils/storage';
+import { authSessionStorage } from '@/utils/storage';
 
 export const loginMockHandler = [
-  rest.post(`${BASE_URL}/api/members/login`, async (req, res, ctx) => {
-    const { email, password } = await req.json<{ email: string; password: string }>();
-
-    const storedUser = userSessionStorage.get();
-
-    if (storedUser && storedUser.email === email && storedUser.password === password) {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          email,
-          token: 'mock-token',
-        })
-      );
-    }
-
+  rest.get(`${BASE_URL}/kakao/login`, (req, res, ctx) => {
+    // 카카오 로그인 성공 후, 서버에서 토큰 발급
     return res(
-      ctx.status(403),
+      ctx.status(200),
       ctx.json({
-        message: 'Invalid email or password',
+        token: 'mock-kakao-token',
       })
     );
   }),
