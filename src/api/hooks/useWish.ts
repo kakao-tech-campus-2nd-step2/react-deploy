f22ffd3f3018c98135f86d45efc36c87bb4d6e78
joojjang <itchu999@naver.com>
2024-08-03 02:15:25 +0900

    feat(API): 위시 삭제 함수 및 훅 구현

--- src/api/hooks/useWish.ts
+++ src/api/hooks/useWish.ts
@@ -37,7 +37,7 @@ const postWish = async (params: WishRequestParams) => {
     if (response.status === 200) {
       console.log('wish posted!');
     } else {
-      console.log('response status:', response.status);
+      console.log('성공은 했는데 200은 아닌: ', response.status);
     }
 
     return null;
@@ -62,5 +62,44 @@ const postWish = async (params: WishRequestParams) => {
 export const usePostWish = (): UseMutationResult<null, Error, WishRequestParams> => {
   return useMutation({
     mutationFn: (params: WishRequestParams) => postWish(params),
+  });
+};
+
+const deleteWish = async (wishId: number) => {
+  try {
+    const response = await fetchInstance.delete(`/api/wishes/${wishId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      console.log('wish deleted!');
+    } else {
+      console.log('성공은 했는데 200은 아닌: ', response.status);
+    }
+
+    return null;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    if (axiosError.response) {
+      // 서버 응답이 있는 경우
+      console.log('Error status:', axiosError.response.status);
+      console.log('Error data:', axiosError.response.data);
+    } else if (axiosError.request) {
+      // 요청이 전송되었지만 응답이 없는 경우
+      console.log('No response received:', axiosError.request);
+    } else {
+      // 요청 설정 중에 발생한 에러
+      console.log('Error message:', axiosError.message);
+    }
+    throw error;
+  }
+};
+
+export const useDeleteWish = (): UseMutationResult<null, Error, number> => {
+  return useMutation({
+    mutationFn: (wishId: number) => deleteWish(wishId),
   });
 };
