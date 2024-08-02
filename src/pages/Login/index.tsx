86221006e7f6c12137b86fa5d07ebd2e3d34bc25
joshuadesu <jeongwongwak@naver.com>
2024-08-02 22:45:41 +0900

    Fixes: 로그인 기능 수정중

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,39 +1,76 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
+import { BASE_URL } from '@/api/instance';
 import KAKAO_LOGO from '@/assets/kakao_logo.svg';
 import { Button } from '@/components/common/Button';
 import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
 import { Spacing } from '@/components/common/layouts/Spacing';
 import { breakpoints } from '@/styles/variants';
-import { authSessionStorage } from '@/utils/storage';
+// import { authSessionStorage } from '@/utils/storage';
 
 export const LoginPage = () => {
-  const [id, setId] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [queryParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
 
-  const handleConfirm = () => {
-    if (!id || !password) {
+  const handleLogin = async () => {
+    if (!email || !password) {
       alert('아이디와 비밀번호를 입력해주세요.');
       return;
     }
 
-    // TODO: API 연동
+    try {
+      const response = await fetch(`${BASE_URL}/api/members/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        });
 
-    // TODO: API 연동 전까지 임시 로그인 처리
-    authSessionStorage.set(id);
+        const result = await response.json();
 
-    const redirectUrl = queryParams.get('redirect') ?? `${window.location.origin}/`;
-    return window.location.replace(redirectUrl);
+        // 아래 블로그에서 인용하였음.
+        // https://velog.io/@jihukimme/React-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-%EB%A1%9C%EA%B7%B8%EC%95%84%EC%9B%83-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+
+        if (response.status === 200) {
+          setIsLogin(false);
+          // Store token in local storage
+          sessionStorage.setItem("token", result.token);
+          sessionStorage.setItem("email", result.email); // 여기서 userid를 저장합니다.
+          sessionStorage.setItem("role", result.role); // 여기서 role를 저장합니다.
+          sessionStorage.setItem("storeid", result.storeId); // 여기서 role를 저장합니다.
+          console.log("로그인성공, 이메일주소:" + result.email);
+
+          const redirectUrl = queryParams.get('redirect') ?? `${window.location.origin}/`;
+          return window.location.replace(redirectUrl);
+        } else {
+          console.log(response)
+        }
+    } catch (error) {
+      console.error("로그인 실패", error);
+    };
+
+
   };
+
+  const goToSignUp = () => {
+    navigate('/signup');
+  }
 
   return (
     <Wrapper>
       <Logo src={KAKAO_LOGO} alt="카카고 CI" />
       <FormWrapper>
-        <UnderlineTextField placeholder="이름" value={id} onChange={(e) => setId(e.target.value)} />
+        <UnderlineTextField placeholder="이름" value={email} onChange={(e) => setEmail(e.target.value)} />
         <Spacing />
         <UnderlineTextField
           type="password"
@@ -48,7 +85,12 @@ export const LoginPage = () => {
             sm: 60,
           }}
         />
-        <Button onClick={handleConfirm}>로그인</Button>
+        <Button onClick={handleLogin}>
+          로그인
+          {isLogin}
+          </Button>
+        <Spacing />
+        <Button onClick={goToSignUp}>회원가입</Button>
       </FormWrapper>
     </Wrapper>
   );
