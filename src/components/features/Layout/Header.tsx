182a2894cde0499c60539a5946fd52dce9c7304b
faddishcorn <juni129461@gmail.com>
2024-08-03 01:26:12 +0900

    feat: 백엔드 배포ip 변경기능

--- src/components/features/Layout/Header.tsx
+++ src/components/features/Layout/Header.tsx
@@ -1,16 +1,39 @@
 import styled from '@emotion/styled';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 import { Container } from '@/components/common/layouts/Container';
 import { useAuth } from '@/provider/Auth';
 import { getDynamicPath, RouterPath } from '@/routes/path';
-
+import { updateBaseURL } from '@/api/instance';
 export const Header = () => {
   const navigate = useNavigate();
   const authInfo = useAuth();
+  const [selectedUser, setSelectedUser] = useState('최유성');
 
   const handleLogin = () => {
     navigate(getDynamicPath.login());
+  };
+
+  const handleUserChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const user = e.target.value;
+    setSelectedUser(user);
+    switch (user) {
+      case '최유성':
+        updateBaseURL('http://3.36.86.203:8080');
+        break;
+      case '오승환':
+        updateBaseURL('http://3.35.176.235:8080');
+        break;
+      case '고승현':
+        updateBaseURL('http://13.209.84.167:8080');
+        break;
+      case '김병수':
+        updateBaseURL('http://13.124.128.255:8080');
+        break;
+      default:
+        updateBaseURL('http://3.36.86.203:8080');
+    }
+    window.location.reload();
   };
 
   return (
@@ -23,6 +46,12 @@ export const Header = () => {
           />
         </Link>
         <RightWrapper>
+          <select value={selectedUser} onChange={handleUserChange}>
+            <option value="최유성">최유성</option>
+            <option value="오승환">오승환</option>
+            <option value="고승현">고승현</option>
+            <option value="김병수">김병수</option>
+          </select>
           {authInfo ? (
             <LinkButton onClick={() => navigate(RouterPath.myAccount)}>내 계정</LinkButton>
           ) : (
@@ -49,7 +78,10 @@ export const Wrapper = styled.header`
 const Logo = styled.img`
   height: ${HEADER_HEIGHT};
 `;
-const RightWrapper = styled.div``;
+const RightWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const LinkButton = styled.p`
   align-items: center;
