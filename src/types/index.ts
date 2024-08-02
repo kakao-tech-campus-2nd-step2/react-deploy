f946ffbc21f35a682f8ba8f93ecfd0dc97031927
dlawlghks <ikuwlghks@naver.com>
2024-08-02 22:32:26 +0900

    feat: 데이터 타입 정의 추가 및 수정

---
+++ src/types/index.ts
@@ -0,0 +1,89 @@
+export type CategoryData = {
+  id: number;
+  name: string;
+  description: string;
+  color: string;
+  imageUrl: string;
+};
+
+export type ProductData = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  categoryId: number;
+};
+
+export type ProductOptionsData = {
+  id: number;
+  name: string;
+  quantity: number;
+  productId: number;
+};
+
+export type GoodsDetailOptionItemData = {
+  key: string;
+  value: string;
+  level: number;
+  options: GoodsDetailOptionItemData[];
+  id?: number;
+  price?: number;
+  stockQuantity: number;
+};
+
+export type OrderHistory = {
+  id: number;
+  count: number;
+};
+
+export type OrderFormData = {
+  productId: number;
+  productQuantity: number;
+  messageCardTextMessage: string;
+  senderId: number;
+  receiverId: number;
+  hasCashReceipt: boolean;
+  cashReceiptType?: 'PERSONAL' | 'BUSINESS';
+  cashReceiptNumber?: string;
+};
+
+export type MessageCardTemplateData = {
+  id: number;
+  defaultTextMessage: string;
+  thumbUrl: string;
+  imageUrl: string;
+};
+
+export type WishlistItem = {
+  id: number;
+  product: {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+  };
+};
+
+export type WishlistResponse = {
+  content: WishlistItem[];
+  pageable: {
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    pageNumber: number;
+    pageSize: number;
+    offset: number;
+    unpaged: boolean;
+    paged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  number: number;
+  size: number;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+};
