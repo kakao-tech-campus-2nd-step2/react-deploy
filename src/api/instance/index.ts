b9a28dacb117cb22446d2bdc424b2fd2c9490f96
HyeonChae1104 <211063@jnu.ac.kr>
2024-08-02 16:34:20 +0900

    feat: 백엔드 API 선택 기능 및 API 적용을 위한 셋팅

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -18,8 +18,26 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
 
 export const BASE_URL = 'https://api.example.com';
 // TODO: 추후 서버 API 주소 변경 필요
+
+interface Engineers {
+  [key: string]: string;
+}
+
+const engineers: Engineers = {
+  '박민규': 'https://backend.lee.com', // 실제 URL로 대체 필요
+  '모아림': 'https://backend.park.com', // 실제 URL로 대체 필요
+  '조홍식': 'https://backend.seo.com',  // 실제 URL로 대체 필요
+};
+
+let currentBaseUrl = engineers['박민규'];
+
+export const setApiBaseUrl = (engineer: string) => {
+  currentBaseUrl = engineers[engineer];
+  fetchInstance.defaults.baseURL = currentBaseUrl;
+};
+
 export const fetchInstance = initInstance({
-  baseURL: 'https://api.example.com',
+  baseURL: currentBaseUrl,
 });
 
 export const queryClient = new QueryClient({
