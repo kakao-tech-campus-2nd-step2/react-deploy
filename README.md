767f850f399c0a6f8c5ed01ce2db89c0593e0252
ychy61 <97201798+ychy61@users.noreply.github.com>
2024-08-04 22:30:29 +0900

    강원대 FE_윤채연 6주차 과제 (#102)
    * feat: add step0 files and update README
    
    * feat: 카테고리 조회 수정
    
    * feat: 상품 조회 훅 수정
    
    * chore: prettier 적용
    
    * feat: auth mock 데이터 ㅅ줭

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# react-deploy
+# 카카오 테크 캠퍼스 - 프론트엔드 카카오 선물하기 편
+
+### 실행 방법
+```
+git clone https://github.com/kakao-tech-campus-2nd-step2/react-deploy week6
+git checkout ychy61
+cd week6
+npm install
+npm run start
+```
+## step0 - 기본 코드 준비
+
+[🔗 link](https://edu.nextstep.camp/s/hazAC9xa/ls/Ipv4p6na)
+
+## 1단계 - API 명세 협의 & 반영
+
+- [ ] 팀 내에 배포 될 API가 여러개 일 경우 상단 네비게이션 바에서 선택 가능하게 함
+> API를 통일하여 구현하지 않음
---
+++ src/hooks/useCurrentCategory.ts
@@ -0,0 +1,24 @@
+import { useMemo } from 'react';
+
+import { useGetCategories } from '@/api/hooks/useGetCategorys';
+import { getCurrentCategory } from '@/components/features/Category/CategoryHeroSection';
+
+type Props = { categoryId: string };
+
+export const useCurrentCategory = ({ categoryId }: Props) => {
+  const { data, isLoading, isError } = useGetCategories();
+
+  const isRender = useMemo(() => {
+    return !isLoading && !isError && data !== undefined;
+  }, [data, isLoading, isError]);
+
+  const currentCategory = useMemo(
+    () => getCurrentCategory(categoryId, data ?? []),
+    [categoryId, data],
+  );
+
+  return {
+    isRender,
+    currentCategory,
+  };
+};
