3d3a929652233d0bbb44ed260d6fc20250ebbe54
joojjang <itchu999@naver.com>
2024-08-05 02:30:08 +0900

    feat(order): 포인트 사용하는 부분 추가

--- src/components/features/Order/OrderForm/OrderInfo/index.tsx
+++ src/components/features/Order/OrderForm/OrderInfo/index.tsx
@@ -18,6 +18,8 @@ export const OrderFormOrderInfo = ({ orderHistory }: Props) => {
 
   const { data: detail } = useGetProductDetail({ productId: productId.toString() });
   const totalPrice = detail.price * count;
+  const discount = 100; // 포인트로 바꾸기
+  const sellingPrice = totalPrice - discount;
 
   return (
     <Wrapper>
@@ -25,18 +27,33 @@ export const OrderFormOrderInfo = ({ orderHistory }: Props) => {
         <HeadingText>결제 정보</HeadingText>
       </Title>
       <Divider color="#ededed" />
-      <CashReceiptFields />
+      <>
+        <CashReceiptFields />
+      </>
       <Divider color="#ededed" />
-
-      <div>총 상품금액 ({count}개)</div>
-      <Divider color="#ededed" />
-      <ItemWrapper>
-        <LabelText>최종 결제금액</LabelText>
-        <HeadingText>{totalPrice}원</HeadingText>
-      </ItemWrapper>
+      <>
+        <Subtitle>
+          <LabelText>결제정보</LabelText>
+        </Subtitle>
+        <Divider color="#ededed" />
+        <ListWrapper>
+          <li className="list_item">
+            <span className="tit_price">총 상품금액 ({count}개)</span>
+            <span className="num_price">{totalPrice}원</span>
+          </li>
+          <li className="list_item">
+            <span className="tit_price">쇼핑포인트 사용</span>
+            <span className="num_price">- {discount}원</span>
+          </li>
+        </ListWrapper>
+        <ItemWrapper>
+          <LabelText>최종 결제금액</LabelText>
+          <HeadingText>{sellingPrice}원</HeadingText>
+        </ItemWrapper>
+      </>
       <Divider color="#ededed" />
       <Spacing height={32} />
-      <Button type="submit">{totalPrice}원 결제하기</Button>
+      <Button type="submit">{sellingPrice}원 결제하기</Button>
     </Wrapper>
   );
 };
@@ -53,10 +70,34 @@ const Title = styled.h6`
   padding: 24px 0 20px;
 `;
 
-const ItemWrapper = styled.div`
+const Subtitle = styled.h3`
   padding: 16px;
-  width: 100%;
+  border-bottom: 1px solid #ededed;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 24px;
+`;
+
+const ListWrapper = styled.ul`
+  padding: 16px 16px 4px;
+  list-style: none;
+  font-size: 14px;
+  line-height: 1.5;
+
+  .list_item {
+    overflow: hidden;
+    padding-bottom: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const ItemWrapper = styled.div`
   display: flex;
+  margin: 0 16px;
+  padding: 16px 0;
+  border-top: 1px solid #f5f5f5;
   justify-content: space-between;
   align-items: center;
 `;
