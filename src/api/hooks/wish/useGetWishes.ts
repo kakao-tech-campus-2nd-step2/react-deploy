63c2248d40a87b51f927c313823eb846e66e90ef
joojjang <itchu999@naver.com>
2024-08-04 20:08:41 +0900

    refactor(getWishesPath): URLSearchParams 객체 사용

--- src/api/hooks/wish/useGetWishes.ts
+++ src/api/hooks/wish/useGetWishes.ts
@@ -7,13 +7,20 @@ import { fetchInstance } from '../../instance';
 type GetWishRequestData = {
   page: number;
   size: number;
-  sort: string;
+  sort?: string;
 };
 
 type GetWishResponseData = WishData[];
 
-export const getWishesPath = ({ page, size, sort }: GetWishRequestData) =>
-  `/api/wishes?page=${page}&size=${size}&sort=${sort}`;
+export const getWishesPath = ({ page = 0, size, sort }: GetWishRequestData) => {
+  const queryParams = new URLSearchParams();
+
+  queryParams.append('page', page.toString());
+  queryParams.append('size', size.toString());
+  queryParams.append('sort', sort || 'createdDate,desc');
+
+  return `/api/wishes?${queryParams.toString()}`;
+};
 
 const getWishes = async (params: GetWishRequestData) => {
   try {
