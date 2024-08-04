a8b423d88418ab2c867f652bff6bf477c1ca1760
joojjang <itchu999@naver.com>
2024-08-05 02:31:17 +0900

    feat(order): 포인트 사용하는 부분 인풋, 전액사용 버튼 추가, 포인트 로직 구현

--- src/components/features/Order/OrderForm/OrderInfo/index.tsx
+++ src/components/features/Order/OrderForm/OrderInfo/index.tsx
@@ -1,5 +1,6 @@
 import { Divider } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 import { useGetProductDetail } from '@/api/hooks/products/useGetProductDetail';
 import { Button } from '@/components/common/Button';
@@ -13,19 +14,61 @@ import { CashReceiptFields } from '../Fields/CashReceiptFields';
 type Props = {
   orderHistory: OrderHistory;
 };
+
 export const OrderFormOrderInfo = ({ orderHistory }: Props) => {
   const { productId, count } = orderHistory;
-
   const { data: detail } = useGetProductDetail({ productId: productId.toString() });
+
   const totalPrice = detail.price * count;
-  const discount = 100; // 포인트로 바꾸기
+  const points = 1000; // 바꾸기
+  const [discount, setDiscount] = useState<number>(100); // 바꾸기
+  const [inputValue, setInputValue] = useState<number | undefined>(undefined);
   const sellingPrice = totalPrice - discount;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (value === '') {
+      setInputValue(undefined);
+      setDiscount(0);
+    } else {
+      const parsedValue = parseInt(value, 10);
+      if (isNaN(parsedValue)) {
+        setInputValue(0);
+        setDiscount(0);
+      } else {
+        setInputValue(parsedValue);
+        setDiscount(parsedValue);
+      }
+    }
+  };
+
+  const handleUseAllPoints = () => {
+    setInputValue(points);
+    setDiscount(points);
+  };
 
   return (
     <Wrapper>
       <Title>
         <HeadingText>결제 정보</HeadingText>
       </Title>
+      <Divider color="#ededed" />
+      <>
+        <Subtitle>
+          <LabelText>쇼핑포인트 사용</LabelText>
+          <span className="point_g">{points} P</span>
+        </Subtitle>
+        <ItemWrapper>
+          <PointInputWrapper>
+            <PointInput type="number" value={inputValue} onChange={handleInputChange} />
+            <span className="ico_point">P</span>
+          </PointInputWrapper>
+          <PointButton type="button" onClick={handleUseAllPoints}>
+            전액사용
+          </PointButton>
+        </ItemWrapper>
+      </>
       <Divider color="#ededed" />
       <>
         <CashReceiptFields />
@@ -66,6 +109,7 @@ const Wrapper = styled.div`
   border-left: 1px solid #ededed;
   border-right: 1px solid #ededed;
   padding: 16px;
+  overflow: scroll;
 `;
 
 const Title = styled.h6`
@@ -78,6 +122,16 @@ const Subtitle = styled.h3`
   font-weight: 700;
   font-size: 15px;
   line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .point_g {
+    font-style: normal;
+    font-weight: 400;
+    float: right;
+    font-size: 14px;
+  }
 `;
 
 const ListWrapper = styled.ul`
@@ -102,4 +156,45 @@ const ItemWrapper = styled.div`
   border-top: 1px solid #f5f5f5;
   justify-content: space-between;
   align-items: center;
+  gap: 6px;
+  position: relative;
+`;
+
+const PointInputWrapper = styled.div`
+  position: relative;
+  flex-grow: 1;
+  font-size: 14px;
+  line-height: 1.5;
+
+  .ico_point {
+    position: absolute;
+    right: 14px;
+    top: 10px;
+    width: 7px;
+    height: 10px;
+  }
+`;
+
+const PointInput = styled.input`
+  display: block;
+  width: 100%;
+  height: 40px;
+  padding: 0 23px 0 42px;
+  border: 1px solid #ededed;
+  border-radius: 2px;
+  box-sizing: border-box;
+  text-align: right;
+  color: #222;
+`;
+
+const PointButton = styled.button`
+  width: 80px;
+  height: 40px;
+  border: 1px solid #ededed;
+  border-radius: 2px;
+  font-size: 14px;
+  line-height: 38px;
+  background-color: #fff;
+  color: #222;
+  margin-left: 8px;
 `;
