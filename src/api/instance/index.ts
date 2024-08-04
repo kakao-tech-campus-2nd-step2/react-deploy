d75ca6611080ba8a3036aa56ea7a243a1494efb6
joojjang <itchu999@naver.com>
2024-08-04 22:53:55 +0900

    refactor(instance): fetchInstance와 initInstance 병합

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from '@tanstack/react-query';
-import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
+import axios, { type AxiosInstance } from 'axios';
 
 import { apiOptionLocalStorage } from '@/utils/storage';
 
@@ -23,22 +23,13 @@ export const BASE_URL =
 
 console.log('BASE_URL: ', BASE_URL); // test
 
-const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
-  const instance = axios.create({
-    timeout: 5000,
-    ...config,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      ...config.headers,
-    },
-  });
-
-  return instance;
-};
-
-export const fetchInstance = initInstance({
+export const fetchInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
 
 export const queryClient = new QueryClient({
