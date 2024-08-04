0c3542e8490b1b55d28f2d345666cae393579042
joojjang <itchu999@naver.com>
2024-08-04 20:09:01 +0900

    chore(usePostWish): 필요 없는 주석 삭제

--- src/api/hooks/wish/usePostWish.ts
+++ src/api/hooks/wish/usePostWish.ts
@@ -9,21 +9,6 @@ type PostWishRequestParams = {
   productId: number;
 };
 
-// type WishResponseParams = null;
-
-// type WishResponseData = WishResponseParams;
-
-// export const getProductsPath = ({ categoryId, pageToken, maxResults }: RequestParams) => {
-//   const params = new URLSearchParams();
-
-//   params.append('categoryId', categoryId);
-//   params.append('sort', 'name,asc');
-//   if (pageToken) params.append('page', pageToken);
-//   if (maxResults) params.append('size', maxResults.toString());
-
-//   return `${BASE_URL}/api/products?${params.toString()}`;
-// };
-
 const token = authSessionStorage.get();
 
 const postWish = async (params: PostWishRequestParams) => {
