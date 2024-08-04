1677d84f8260fa35ea57c11162338f3072507b0a
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-08-04 15:07:47 +0900

    Add environment variables for API URL

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -4,8 +4,9 @@ import axios from 'axios';
 
 import { authSessionStorage } from '@/utils/storage';
 
+const API_URL = process.env.REACT_APP_API_URL;
 // 서버 API 주소
-export const BASE_URL = 'http://43.201.17.220:8080';
+//export const BASE_URL = 'http://43.201.17.220:8080';
 // 다른 서버 주소
 // // 명준
 // http://13.125.199.167:8080 준형
@@ -44,7 +45,7 @@ const initInstance = (axiosConfig: AxiosRequestConfig): AxiosInstance => {
 
 // 기본 서버 API 주소 설정
 export const fetchInstance = initInstance({
-  baseURL: BASE_URL,
+  baseURL: API_URL,
 });
 
 export const setBaseURL = (url: string) => {
