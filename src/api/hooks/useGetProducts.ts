87c06b706d79fef0b84e6717527101bb3200c54e
Eric <real.dassy@gmail.com>
2024-08-05 13:04:11 +0900

    fix: api 명세 변경에 따른 네이밍 변경

--- src/api/hooks/useGetProducts.ts
+++ src/api/hooks/useGetProducts.ts
@@ -9,42 +9,44 @@ import type { ProductData } from '@/types';
 
 import { fetchInstance } from '../instance';
 
-type RequestParams = {
+interface RequestParams {
   categoryId: string;
-  pageToken?: string;
-  maxResults?: number;
-};
+  sort?: string;
+  page: number;
+  size?: number;
+}
 
-type ProductsResponseData = {
+interface ProductsResponseData {
   products: ProductData[];
   nextPageToken?: string;
   pageInfo: {
     totalResults: number;
     resultsPerPage: number;
   };
-};
+}
 
-type ProductsResponseRawData = {
-  content: ProductData[];
+interface ProductsResponseRawData {
+  products: ProductData[];
   number: number;
   totalElements: number;
   size: number;
   last: boolean;
-};
+}
 
 export const getProducts = async (params: RequestParams): Promise<ProductsResponseData> => {
   const response = await fetchInstance.get<ProductsResponseRawData>(ApiPath.products.root, {
     params: {
       categoryId: params.categoryId,
-      sort: 'createdData,desc',
-      page: params.pageToken,
-      size: params.maxResults,
+      sort: params.sort || 'id,desc',
+      page: params.page,
+      size: params.size || 20,
     },
   });
   const data = response.data;
+  console.log(data);
 
   return {
-    products: data.content,
+    products: data.products,
     nextPageToken: data.last === false ? (data.number + 1).toString() : undefined,
     pageInfo: {
       totalResults: data.totalElements,
@@ -53,16 +55,16 @@ export const getProducts = async (params: RequestParams): Promise<ProductsRespon
   };
 };
 
-type Params = Pick<RequestParams, 'maxResults' | 'categoryId'> & { initPageToken?: string };
+type Params = Pick<RequestParams, 'size' | 'categoryId'> & { initPageToken?: string };
 export const useGetProducts = ({
   categoryId,
-  maxResults = 20,
+  size = 20,
   initPageToken,
 }: Params): UseInfiniteQueryResult<InfiniteData<ProductsResponseData>> => {
   return useInfiniteQuery({
-    queryKey: ['products', categoryId, maxResults, initPageToken],
+    queryKey: ['products', categoryId, size, initPageToken],
     queryFn: async ({ pageParam = initPageToken }) => {
-      return getProducts({ categoryId, pageToken: pageParam, maxResults });
+      return getProducts({ categoryId, page: parseInt(pageParam ?? '0', 10), size });
     },
     initialPageParam: initPageToken,
     getNextPageParam: (lastPage) => lastPage.nextPageToken,
