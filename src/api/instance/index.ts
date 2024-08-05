735601adb0dee466a9a67fa093a22c44688bec16
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-08-06 02:41:29 +0900

    refactor. 서버 바꿈

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import { authSessionStorage } from '@/utils/storage';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://kakao-gift.43.201.17.220.sslip.io/';
+const API_URL = process.env.REACT_APP_API_URL || 'https://kakaogift.shop';
 
 const initInstance = (axiosConfig: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
