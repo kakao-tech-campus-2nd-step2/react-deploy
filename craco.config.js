606da689d511d0df2383276e35fcc566ecf85653
Hyoeunkh <lhe4528@knu.ac.kr>
2024-08-05 14:15:48 +0900

    fix: craco env 설정 삭제

--- craco.config.js
+++ craco.config.js
@@ -5,12 +5,6 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.REACT_APP_API': JSON.stringify(process.env.REACT_APP_API),
-        'process.env.REACT_APP_PUBLIC_URL': JSON.stringify(process.env.REACT_APP_PUBLIC_URL),
-      }),
-    ],
   },
   jest: {
     configure: {
