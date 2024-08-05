caddd05908b4ab321c469361baf2ef5ab5bcfd66
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-08-06 03:45:17 +0900

    refactor. error 해결

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,84 +17,89 @@ import SignUpPage from '@/pages/SignUp';
 import { PrivateRoute } from './components/PrivateRoute';
 import { RouterPath } from './path';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: RouterPath.root,
+      element: <Layout />,
+      children: [
+        {
+          path: RouterPath.home,
+          element: <HomePage />,
+        },
+        {
+          path: RouterPath.category,
+          element: <CategoryPage />,
+        },
+        {
+          path: RouterPath.addProduct,
+          element: <ProductAddPage />,
+        },
+        {
+          path: RouterPath.addCategory,
+          element: <CategoryAddPage />,
+        },
+        {
+          path: RouterPath.editCategory,
+          element: <CategoryEditPage />,
+        },
+        {
+          path: RouterPath.editProduct,
+          element: <ProductEditPage />,
+        },
+        {
+          path: RouterPath.productsDetail,
+          element: <GoodsDetailPage />,
+        },
+        {
+          path: RouterPath.myAccount,
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: RouterPath.myAccount,
+              element: <MyAccountPage />,
+            },
+          ],
+        },
+        {
+          path: RouterPath.order,
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: RouterPath.order,
+              element: <OrderPage />,
+            },
+          ],
+        },
+        {
+          path: RouterPath.favorites,
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: RouterPath.favorites,
+              element: <FavoritesPage />,
+            },
+          ],
+        },
+        {
+          path: RouterPath.notFound,
+          element: <Navigate to={RouterPath.home} />,
+        },
+      ],
+    },
+    {
+      path: RouterPath.login,
+      element: <LoginPage />,
+    },
+    {
+      path: RouterPath.signUp,
+      element: <SignUpPage />,
+    },
+  ],
   {
-    path: RouterPath.root,
-    element: <Layout />,
-    children: [
-      {
-        path: RouterPath.home,
-        element: <HomePage />,
-      },
-      {
-        path: RouterPath.category,
-        element: <CategoryPage />,
-      },
-      {
-        path: RouterPath.addProduct,
-        element: <ProductAddPage />,
-      },
-      {
-        path: RouterPath.addCategory,
-        element: <CategoryAddPage />,
-      },
-      {
-        path: RouterPath.editCategory,
-        element: <CategoryEditPage />,
-      },
-      {
-        path: RouterPath.editProduct, // 수정 페이지 라우트 추가
-        element: <ProductEditPage />,
-      },
-      {
-        path: RouterPath.productsDetail,
-        element: <GoodsDetailPage />,
-      },
-      {
-        path: RouterPath.myAccount,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.myAccount,
-            element: <MyAccountPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.order,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.order,
-            element: <OrderPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.favorites, // favorites 경로 추가
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.favorites,
-            element: <FavoritesPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.notFound,
-        element: <Navigate to={RouterPath.home} />,
-      },
-    ],
+    basename: '/react-deploy',
   },
-  {
-    path: RouterPath.login,
-    element: <LoginPage />,
-  },
-  {
-    path: RouterPath.signUp,
-    element: <SignUpPage />,
-  },
-]);
+);
 
 export const Routes = () => {
   return <RouterProvider router={router} />;
