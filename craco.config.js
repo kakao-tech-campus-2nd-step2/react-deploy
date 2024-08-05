0ea3025b537cfa1c842e2e947b9e33c42632fa7e
Hyoeunkh <lhe4528@knu.ac.kr>
2024-08-05 14:13:45 +0900

    feat: 환경변수 설정 추가

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,12 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_API': JSON.stringify(process.env.REACT_APP_API),
+        'process.env.REACT_APP_PUBLIC_URL': JSON.stringify(process.env.REACT_APP_PUBLIC_URL),
+      }),
+    ],
   },
   jest: {
     configure: {
