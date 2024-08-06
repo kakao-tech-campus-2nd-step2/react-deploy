41ca855254cf2bc1974d853f3e2894a5b9053477
anheejeong <eyrt6973@naver.com>
2024-08-06 14:51:38 +0900

    [Chore] 배포 환경 설정

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,22 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: (webpackConfig) => {
+      // Babel loader를 통해 ESM 모듈을 변환하는 설정 추가
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        include: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+        exclude: /node_modules\/(?!strip-ansi)/, // 특정 모듈만 포함시키기
+      });
+
+      return webpackConfig;
+    },
   },
 };
